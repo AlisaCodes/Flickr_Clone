9c3e51cc2e024bbbdf306a6960e67d7ade45de79
lindsay culver & alisa colon <student@epicodus.com>
2015-10-21 16:20:10 -0700

    add avatar

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,5 +1,6 @@
 class LandingController < ActionController::Base
   before_action :authenticate_user!, except: [:index]
   def index
+    @users = User.all
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
 
   # attr_accessible :email, :password, :password_confirmation, :remember_me
 
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100#" }, :default_url => "/images/:style/missing.png"
-  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
-  end
+    has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100#" }, :default_url => "/images/:style/missing.png"
+    validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+
 end
