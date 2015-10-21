2689c75f0d2ebc886f7659a21a153eb82b09f57d
lindsay culver & alisa colon <student@epicodus.com>
2015-10-21 16:31:04 -0700

    add user authentication, logout and login and sign up

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,6 +1,6 @@
-class LandingController < ActionController::Base
+class LandingController < ApplicationController
   before_action :authenticate_user!, except: [:index]
   def index
-    @users = User.all
+
   end
 end
